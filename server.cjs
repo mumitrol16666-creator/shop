c9f3bb6b2eead9e5dca6bb079d42730f4d7881df
mumitrol16666-creator <mumitrol16666@gmail.com>
2026-08-20 20:25:13 +0500

    Serve uploaded WebP images with correct MIME type

--- server.cjs
+++ server.cjs
@@ -28,6 +28,8 @@ const MIME_TYPES = {
   ".json": "application/json; charset=utf-8",
   ".png": "image/png",
   ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".webp": "image/webp",
   ".svg": "image/svg+xml",
   ".woff2": "font/woff2",
   ".woff": "font/woff",
